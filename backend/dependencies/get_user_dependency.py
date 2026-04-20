2425e60f5dfdf76b74b54c553d8f9ff4668a3dfa
TheSushantKumbhar <me.sushantkumbhar@gmail.com>
2026-04-20 21:42:47 +0530

    adding user depedencies

--- backend/dependencies/get_user_dependency.py
+++ backend/dependencies/get_user_dependency.py
@@ -29,7 +29,6 @@ async def get_current_user(
             detail="Invalid token payload"
         )
 
-    # ✅ FIXED HERE (no int)
     result = await db.execute(select(User).where(User.id == user_id))
     user = result.scalar_one_or_none()
 
