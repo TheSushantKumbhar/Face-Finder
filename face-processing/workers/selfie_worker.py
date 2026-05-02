c3b4a70628ce02cd433405bf30397756c04313f6
aditya-sutar-45 <aditya1.sutar@outlook.com>
2026-05-02 21:56:00 +0530

    added face detection and ingestion pipeline for the selfie consumer

--- face-processing/workers/selfie_worker.py
+++ face-processing/workers/selfie_worker.py
@@ -1,7 +1,9 @@
 import json
-
+from services.vector_store import index
 from rabbitmq.connection import create_channel
 from config import SELFIE_QUEUE_NAME
+from services.pipeline import process_selfie
+from utils.image_loader import download_image, image_cleanup
 
 
 connection, channel = create_channel()
@@ -9,7 +11,39 @@ connection, channel = create_channel()
 
 def selfie_callback(ch, method, properties, body):
     data = json.loads(body)
-    print(data)
+
+    img_url = data["r2URL"]
+    selfie_id = data["selfieID"]
+
+    print(f"INFO received img url: {img_url}")
+    print(f"INFO received selfie id: {selfie_id}")
+
+    img_path = None
+
+    try:
+        img_path = download_image(
+            url=img_url,
+            folder="temp",
+        )
+
+        print(f"INFO downloaded image to path: {img_path}")
+
+        process_selfie(
+            index=index,
+            path=img_path,
+            selfie_id=selfie_id,
+        )
+
+        print("\n")
+        print(f"INFO indexed faces from \nselfie id: {selfie_id}\nurl: {img_url}")
+        print("\n")
+
+        ch.basic_ack(delivery_tag=method.delivery_tag)
+    except Exception as e:
+        print(f"ERROR processing the selfie: {e}")
+        ch.basic_nack(delivery_tag=method.delivery_tag, requeue=False)
+    finally:
+        image_cleanup(path=img_path)
 
 
 channel.basic_qos(prefetch_count=1)
